bd52ee58211c1b7e541c62fd2496a8a75d82fbf0
KxK <mr.ygorvaz@gmail.com>
2023-06-01 20:58:35 -0300

    Created blank class to be used as ViewModel of the controller settings

---
+++ ControlApp/MVVM/VMSettingsGroups/GroupSettingsVM.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Nefarius.DsHidMini.ControlApp.DSHM_Settings;
+using Nefarius.DsHidMini.ControlApp.UserData;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reactive;
+using System.Text.Json.Serialization;
+using System.Windows;
+
+namespace Nefarius.DsHidMini.ControlApp.MVVM
+{
+    public abstract class GroupSettingsVM : ReactiveObject
+    {
+
+
+    }
+
+
+}
