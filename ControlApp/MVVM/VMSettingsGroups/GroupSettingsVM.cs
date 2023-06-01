705225bdcadb71c9f8f4625a7d2d17e8899fa686
KxK <mr.ygorvaz@gmail.com>
2023-06-01 20:59:42 -0300

    Added dictionary to be used to find the Header of each settings groups

--- ControlApp/MVVM/VMSettingsGroups/GroupSettingsVM.cs
+++ ControlApp/MVVM/VMSettingsGroups/GroupSettingsVM.cs
@@ -15,7 +15,26 @@ namespace Nefarius.DsHidMini.ControlApp.MVVM
 {
     public abstract class GroupSettingsVM : ReactiveObject
     {
+        /// Replace with LexLoc
+        private static Dictionary<SettingsModeGroups, string> DictGroupHeader = new()
+        {
+            { SettingsModeGroups.LEDsControl, "LEDs control" },
+            { SettingsModeGroups.WirelessSettings, "Wireless settings" },
+            { SettingsModeGroups.SticksDeadzone, "Sticks DeadZone (DZ)" },
+            { SettingsModeGroups.RumbleGeneral, "Rumble settings" },
+            { SettingsModeGroups.OutputReportControl, "Output report control" },
+            { SettingsModeGroups.RumbleLeftStrRescale, "Left motor (heavy) rescale" },
+            { SettingsModeGroups.RumbleRightConversion, "Variable light rumble emulation adjuster" },
+            { SettingsModeGroups.Unique_All, "Mode specific settings" },
+            { SettingsModeGroups.Unique_Global, "Default settings" },
+            { SettingsModeGroups.Unique_General, "General settings" },
+            { SettingsModeGroups.Unique_SDF, "SDF mode specific settings" },
+            { SettingsModeGroups.Unique_GPJ, "GPJ mode specific settings" },
+            { SettingsModeGroups.Unique_SXS, "SXS mode specific settings" },
+            { SettingsModeGroups.Unique_DS4W, "DS4W mode specific settings" },
+            { SettingsModeGroups.Unique_XInput, "GPJ mode specific settings" },
 
+        };
 
     }
 
