c38b518332ea901c50978fecaae994897d40d918
KxK <mr.ygorvaz@gmail.com>
2023-06-02 14:33:51 -0300

    Added WPF converter utilities

---
+++ ControlApp/Util/WPF/BooleanToReverseConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Avalonia.Data.Converters;
+using System.Globalization;
+using Avalonia.Markup.Xaml;
+
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Avalonia.Markup.Xaml.Templates;
+using System.Collections.Generic;
+using Nefarius.DsHidMini.ControlApp.MVVM;
+using Serilog.Formatting.Display;
+
+namespace Nefarius.DsHidMini.ControlApp.Util.WPF
+{
+    public class BooleanToReverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            return !(bool?)value ?? true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+         => !(value as bool?);
+    }
+}
