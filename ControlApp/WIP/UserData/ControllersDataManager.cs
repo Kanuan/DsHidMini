a737835713b4a7171b6a5769d1acfcdaef36eebd
KxK <mr.ygorvaz@gmail.com>
2023-06-02 13:32:04 -0300

    Changed namespace

--- ControlApp/WIP/UserData/ControllersDataManager.cs
+++ ControlApp/WIP/UserData/ControllersDataManager.cs
@@ -10,7 +10,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
-namespace Nefarius.DsHidMini.ControlApp.WIP.UserData
+namespace Nefarius.DsHidMini.ControlApp.UserData
 {
     internal class ControllersDataManager
     {
