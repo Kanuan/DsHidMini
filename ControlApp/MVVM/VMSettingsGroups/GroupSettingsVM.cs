f50a427f794ee7152967c258117f38871f809d9e
KxK <mr.ygorvaz@gmail.com>
2023-06-01 21:08:48 -0300

    Implemented GroupSettingVM abstract class and container class for all group settings

--- ControlApp/MVVM/VMSettingsGroups/GroupSettingsVM.cs
+++ ControlApp/MVVM/VMSettingsGroups/GroupSettingsVM.cs
@@ -13,7 +13,19 @@ using System.Windows;
 
 namespace Nefarius.DsHidMini.ControlApp.MVVM
 {
-    public abstract class GroupSettingsVM : ReactiveObject
+    public class VMGroupsContainer : ReactiveObject
+    {
+        [Reactive] internal List<GroupSettingsVM> GroupSettingsList { get; set; } = new();
+        [Reactive] public SettingsContext Context { get; set; }
+
+        public VMGroupsContainer(BackingDataContainer dataContainer)
+        {
+            Context = dataContainer.modesUniqueData.SettingsContext;
+        }
+
+    }
+
+        public abstract class GroupSettingsVM : ReactiveObject
     {
         /// Replace with LexLoc
         private static Dictionary<SettingsModeGroups, string> DictGroupHeader = new()
@@ -35,6 +47,46 @@ namespace Nefarius.DsHidMini.ControlApp.MVVM
             { SettingsModeGroups.Unique_XInput, "GPJ mode specific settings" },
 
         };
+
+        [Reactive] public virtual bool IsGroupLocked { get; set; } = false;
+
+        [Reactive] public VMGroupsContainer SettingsContainer { get; set; }
+
+        [Reactive] public SettingsContext Context { get; internal set; } = SettingsContext.XInput;
+
+        [Reactive] public virtual bool IsOverrideCheckboxVisible { get; set; } = false;
+
+        public abstract SettingsModeGroups Group { get; }
+
+        public string Header { get; }
+
+
+
+        // Some GroupSettings, like SticksDeadzone, require overriding this since they change behavior depending on the context
+        public virtual void ChangeContext(SettingsContext context)
+        {
+            Context = context;
+        }
+
+        public abstract void ResetGroupToOriginalDefaults();
+
+        public bool ShouldGroupBeEnabledOnReset()
+        {
+            return true;
+        }
+
+        public abstract void LoadSettingsFromBackingDataContainer(BackingDataContainer dataContainerSource);
+        public abstract void SaveSettingsToBackingDataContainer(BackingDataContainer dataContainerSource);
+
+        public GroupSettingsVM(BackingDataContainer backingDataContainer, VMGroupsContainer vmGroupsContainter)
+        {
+            SettingsContainer = vmGroupsContainter;
+            if (DictGroupHeader.TryGetValue(Group, out string groupHeader)) Header = groupHeader;
+            LoadSettingsFromBackingDataContainer(backingDataContainer);
+            ResetGroupToDefaultsCommand = ReactiveCommand.Create(ResetGroupToOriginalDefaults);
+        }
+
+        public ReactiveCommand<Unit, Unit> ResetGroupToDefaultsCommand { get; }
 
     }
 
