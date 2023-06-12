0f3598f755b766883b842a612027f47d563a5b54
KxK <mr.ygorvaz@gmail.com>
2023-06-11 23:18:35 -0300

    Look for connected devices upon app initialization

--- ControlApp/MainWindow.axaml.cs
+++ ControlApp/MainWindow.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia.Controls;
 using Avalonia.Threading;
+using Nefarius.DsHidMini.ControlApp.Drivers;
 using Nefarius.DsHidMini.ControlApp.MVVM;
+using Nefarius.Utilities.DeviceManagement.PnP;
 using ReactiveUI;
 
 namespace ControlApp
@@ -11,6 +13,11 @@ namespace ControlApp
         public MainWindow()
         {
             RxApp.MainThreadScheduler = AvaloniaScheduler.Instance;
+
+            var instance = 0;
+            while (Devcon.Find(DsHidMiniDriver.DeviceInterfaceGuid, out var path, out var instanceId, instance++))
+                _vm.Devices.Add(new TestViewModel(PnPDevice.GetDeviceByInstanceId(instanceId)));
+
             this.DataContext= _vm;
             InitializeComponent();
         }
