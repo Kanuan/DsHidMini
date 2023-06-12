4a2738e885a17ff84ed1cfc4f75ce1bd4ae00713
KxK <mr.ygorvaz@gmail.com>
2023-06-11 23:17:44 -0300

    Set MainWindows view model

--- ControlApp/MainWindow.axaml.cs
+++ ControlApp/MainWindow.axaml.cs
@@ -1,14 +1,17 @@
 using Avalonia.Controls;
 using Avalonia.Threading;
+using Nefarius.DsHidMini.ControlApp.MVVM;
 using ReactiveUI;
 
 namespace ControlApp
 {
     public partial class MainWindow : Window
     {
+        private readonly MainViewModel _vm = new MainViewModel();
         public MainWindow()
         {
             RxApp.MainThreadScheduler = AvaloniaScheduler.Instance;
+            this.DataContext= _vm;
             InitializeComponent();
         }
     }
